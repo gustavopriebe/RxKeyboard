f957e8d264f2ae5f4e52441dbc28dbabd5aef71e
Gustavo Cruz Priebe <gustavo.priebe@unicred.com.br>
2018-10-11 13:17:00 -0300

    Carthage support

---
+++ RxKeyboard/RxKeyboard.h
@@ -0,0 +1,19 @@
+//
+//  RxKeyboard.h
+//  RxKeyboard
+//
+//  Created by Gustavo Priebe on 11/10/18.
+//  Copyright © 2018 Gustavo Priebe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxKeyboard.
+FOUNDATION_EXPORT double RxKeyboardVersionNumber;
+
+//! Project version string for RxKeyboard.
+FOUNDATION_EXPORT const unsigned char RxKeyboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxKeyboard/PublicHeader.h>
+
+
